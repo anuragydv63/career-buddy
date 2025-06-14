d60af0364b6655746aedb7d9793bce4db5339a77
Anurag Yadav <anuragyadav807780@gmail.com>
2025-06-14 15:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 from datetime import datetime
+import os 
 
 app = Flask(__name__)
 app.secret_key = 'careerbuddysecret'
@@ -163,4 +164,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port,debug=True)
